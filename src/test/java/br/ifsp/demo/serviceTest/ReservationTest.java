23605a81cae081f3395d0ef5ab8ac76b406a5a1e
Abel Baes <abel.baes77@gmail.com>
2025-10-08 21:31:28 -0300

    test(reservationservice): adding non active reservation test for cancel reservation method.

--- src/test/java/br/ifsp/demo/serviceTest/ReservationTest.java
+++ src/test/java/br/ifsp/demo/serviceTest/ReservationTest.java
@@ -771,4 +771,24 @@ public class ReservationTest {
                 .isInstanceOf(NoSuchElementException.class)
                 .hasMessageContaining("Reservation not found for id: H-20251005223045384920");
     }
+
+    @DisplayName("Should not be possible to cancel a non existent reservation")
+    @ParameterizedTest(name = "Status = {0}")
+    @CsvSource({"FINALIZED", "CANCELED"})
+    @Tag("UnitTest")
+    @Tag("TDD")
+    void shouldNotBePossibleToCancelANonActiveReservation(ReservationStatus invalidStatus){
+        Room room = new Room("101", RoomStatus.AVAILABLE, 250.0);
+        Guest guest = new Guest("Lucas", 38, "78609833038");
+        StayPeriod stayPeriod = new StayPeriod(LocalDateTime.of(2025, 8, 1, 0, 0),
+                LocalDateTime.of(2025, 10, 10, 0, 0));
+
+        Reservation nonActiveReservation = new Reservation("H-20251005223045384920",room,guest,stayPeriod,invalidStatus);
+
+        fakeRepository.save(nonActiveReservation);
+
+        assertThatThrownBy(() -> sut.cancelReservation("H-20251005223045384920"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("Reservation must be Active");
+    }
 }
